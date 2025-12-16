c32074e99a6c354bdcc16c7a2d2f390e68be35d0
Yahia Ghallale <y.ghallale@studenti.unibs.it>
2025-12-16 13:20:57 +0100

    Update studente.routes.js

--- src/routes/studente.routes.js
+++ src/routes/studente.routes.js
@@ -93,7 +93,7 @@ router.get('/', isAuthenticated, isStudente, async (req,res)=>{
           tentativi,
           media:media,
           cfuTotali,
-          totTentativi: totaleTentativi,
+          totTentativi: esamiVerbalizzati.rows.length,
           esamiVerbalizzati: esamiVerbalizzati.rows
         });
       
